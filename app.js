b5aaba7f42a350261f71dffe802e16127fff1f8e
Nishant Maurya <74682951+nmnishant@users.noreply.github.com>
2022-01-06 22:46:47 +0530

    Handled undefined routes

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ app.use(express.static("./public"));
 // **** ITS A VERY SIMPLE EXPRESS APP (Photify - ClassPlus InternShip Assignment) ****
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// No routes defination is required for this project
 // No middlewares are required for this project
 // No security is required for this project - Its a Static page
+
+app.get("/home", (req, res) =>
+  res.sendFile("./public/index.html", { root: __dirname })
+);
+
+app.all("*", (req, res) =>
+  res.sendFile("./public/error.html", { root: __dirname })
+);
 
 // Please check the public/js for the implementation part
 
